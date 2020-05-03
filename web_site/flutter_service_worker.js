744341f0970a5195da2aa04e7a0e5f7fcbe8710a
Awaleed <a.waleed0011@gmail.com>
2020-05-03 20:03:55 +0200

    refreshing hompage

--- web_site/flutter_service_worker.js
+++ web_site/flutter_service_worker.js
@@ -1,18 +1,20 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "3afa81b1d715a7a3cdca2f10b5b9e52d",
 "assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/images/background_1.jpg": "18b46b3e9fe20e5d255a708c7b31bec5",
+"assets/images/background_2.jpg": "9cac8be1e061e684cb449f40e36323b7",
+"assets/LICENSE": "53731c4d5aebedd0e6f56d3e779c9707",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0b7cd7bd19ce3131def93b241ad06ecc",
-"/": "0b7cd7bd19ce3131def93b241ad06ecc",
-"main.dart.js": "dc0dc0c46314088a74a9bcd0bf9881ba",
-"manifest.json": "3f42f97dbb0a20908cd7a8918c263c34"
+"index.html": "c9937b022019ef05381afec4989bb701",
+"/": "c9937b022019ef05381afec4989bb701",
+"main.dart.js": "cce0bd9b952f84b82f081a72e265eb14",
+"manifest.json": "6e2b0b8f01953390a6eb22d1e2897e09"
 };
 
 self.addEventListener('activate', function (event) {
